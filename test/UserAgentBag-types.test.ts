532827488f27027644eeaf205c4ac21cfa1c48f3
Chuan Ji <chuan@jichu4n.com>
2019-07-15 22:15:15 -0700

    Respond to comments.

--- test/UserAgentBag-types.test.ts
+++ test/UserAgentBag-types.test.ts
@@ -1,14 +1,14 @@
-import UserAgentBag = require('..');
-import { Entry } from '..';
-import test from 'ava';
+import { Entry } from '..'
+import test from 'ava'
+import UserAgentBag = require('..')
 
 test('UserAgentBag types', t => {
-  const bag1 = new UserAgentBag('Foo/fooVersion (comment (nested)!) Bar Baz/bazVersion (comment2) Bar/secondBar');
-  t.assert(bag1.has('Foo'));
+  const bag1 = new UserAgentBag('Foo/fooVersion (comment (nested)!) Bar Baz/bazVersion (comment2) Bar/secondBar')
+  t.assert(bag1.has('Foo'))
   const bag2 = new UserAgentBag([
     ['Foo', '1'],
     ['Bar', '2'],
-    ['Baz', '3'],
-  ] as Array<Entry>);
-  t.assert(bag2.has('Foo'));
-});
+    ['Baz', '3']
+  ] as Array<Entry>)
+  t.assert(bag2.has('Foo'))
+})
