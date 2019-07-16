9f32ee74843a3efcd9604d200ca30eb5e9c53970
Chuan Ji <chuan@jichu4n.com>
2019-07-15 20:22:44 -0700

    Add TypeScript type definitions and simple test.

---
+++ test/UserAgentBag-types.test.ts
@@ -0,0 +1,14 @@
+import UserAgentBag = require('..');
+import { Entry } from '..';
+import test from 'ava';
+
+test('UserAgentBag types', t => {
+  const bag1 = new UserAgentBag('Foo/fooVersion (comment (nested)!) Bar Baz/bazVersion (comment2) Bar/secondBar');
+  t.assert(bag1.has('Foo'));
+  const bag2 = new UserAgentBag([
+    ['Foo', '1'],
+    ['Bar', '2'],
+    ['Baz', '3'],
+  ] as Array<Entry>);
+  t.assert(bag2.has('Foo'));
+});
